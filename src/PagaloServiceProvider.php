1f35c34eddeee232997fe092263b5974144cadf6
Hsing-Li <chang@hsing.li>
2018-11-21 21:37:14 -0600

    fixed config path

--- src/PagaloServiceProvider.php
+++ src/PagaloServiceProvider.php
@@ -7,8 +7,8 @@ class PagaloServiceProvider extends ServiceProvider {
 	protected $defer = false;
 
 	public function boot() {
-		$this->mergeConfigFrom(__DIR__ . '/config/pagalo.php', 'pagalo');
+		$this->mergeConfigFrom(__DIR__ . '/../config/pagalo.php', 'pagalo');
 
-		$this->publishes([__DIR__ . '/config/pagalo.php' => config_path('pagalo.php')], 'config');
+		$this->publishes([__DIR__ . '/../config/pagalo.php' => config_path('pagalo.php')], 'config');
 	}
 }
